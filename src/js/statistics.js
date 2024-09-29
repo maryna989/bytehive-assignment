705e08b0c516804a7793143b3e1d0bd1e843abe8
Maryna Baziliuk <marina.mus24@gmail.com>
2024-09-29 20:37:36 +0300

    Add statistics section with dynamic HTML generation

---
+++ src/js/statistics.js
@@ -0,0 +1,27 @@
+const statistics = [
+  { title: 'People', value: '200' },
+  { title: 'Our users the world', value: '50' },
+  { title: 'Succesfull projects', value: '100' },
+];
+
+const buildStats = statistics
+  .map((item, index) => {
+    let formatValue = item.value;
+
+    if (index === 0) {
+      formatValue = `${formatValue}+`;
+    } else if (index === 1) {
+      formatValue = `${formatValue}K`;
+    } else if (index === 2) {
+      formatValue = `${formatValue}%`;
+    }
+    return `<li class="static-item"><h3>${item.title}</h3><p class="item-value">${formatValue}</p></li>`;
+  })
+  .join('');
+
+const createStats = `<ul class="statistics-list">${buildStats}</ul>`;
+
+const statisContainer = document.querySelector('.js-statistics');
+const titleStatis = document.querySelector('#js-statis');
+
+statisContainer.insertAdjacentHTML('beforeend', createStats);
