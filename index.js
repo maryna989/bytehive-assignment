d62b49cb5455943eec10cfa99063353f7363ad47
maryna989 <marina.mus24@gmail.com>
2024-09-30 11:47:27 +0000

    Deploying to gh-pages from @ maryna989/bytehive-assignment@76ef811b6e286774647a173092e57e82643faa8c 🚀

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();const c=[{title:"About",link:"#"},{title:"Pricing",link:"#"},{title:"Contact",link:"#"}],a=[{platform:"Twitter",link:"#",icon:"#icon-twitter"},{platform:"Facebook",link:"#",icon:"#icon-facebook"},{platform:"Instagram",link:"#",icon:"#icon-instagram"}],u=c.map(t=>`<li class="nav-item"><a href="${t.link}">${t.title}</a></li>`).join(""),f=`
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const n of i.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function o(e){if(e.ep)return;e.ep=!0;const i=r(e);fetch(e.href,i)}})();const a=[{title:"About",link:"#"},{title:"Pricing",link:"#"},{title:"Contact",link:"#"}],l=[{platform:"Twitter",link:"#",icon:"#icon-twitter"},{platform:"Facebook",link:"#",icon:"#icon-facebook"},{platform:"Instagram",link:"#",icon:"#icon-instagram"}],u=a.map(t=>`<li class="nav-item"><a href="${t.link}">${t.title}</a></li>`).join(""),d=`
       <nav class="nav-header">
         <ul class="nav-list">
           ${u}
         </ul>
       </nav>
-    `,d=`
+    `,f=`
   <ul class="social-icons-list">
-    ${a.map(t=>`
+    ${l.map(t=>`
       <li class="social-item">
         <a href="${t.link}" aria-label="${t.platform}">
           <svg class="icon js-icon-${t.platform.toLowerCase()}" width="26" height="26">
@@ -16,5 +16,13 @@
       </li>
     `).join("")}
   </ul>
-`,l=document.querySelector("#header");document.querySelector(".js-logo");l.insertAdjacentHTML("afterbegin",f);l.insertAdjacentHTML("beforeend",d);const m=[{title:"People",value:"200"},{title:"Our users the world",value:"50"},{title:"Succesfull projects",value:"100"}],p=m.map((t,o)=>{let i=t.value;return o===0?i=`${i}+`:o===1?i=`${i}K`:o===2&&(i=`${i}%`),`<li class="static-item"><h3>${t.title}</h3><p class="item-value">${i}</p></li>`}).join(""),h=`<ul class="statistics-list">${p}</ul>`,g=document.querySelector(".js-statistics");document.querySelector("#js-statis");g.insertAdjacentHTML("beforeend",h);
+`,c=document.querySelector("#header");document.querySelector(".js-logo");c.insertAdjacentHTML("afterbegin",d);c.insertAdjacentHTML("beforeend",f);const m=[{title:"People",value:"200"},{title:"Our users the world",value:"50"},{title:"Succesfull projects",value:"100"}],v=m.map((t,s)=>{let r=t.value;return s===0?r=`${r}+`:s===1?r=`${r}K`:s===2&&(r=`${r}%`),`<li class="static-item"><h3>${t.title}</h3><p class="item-value">${r}</p></li>`}).join(""),g=`<ul class="statistics-list">${v}</ul>`,p=document.querySelector(".js-statistics");document.querySelector("#js-statis");p.insertAdjacentHTML("beforeend",g);const h=[["Creative Direction","Art Direction","Visual Design","Service Design"],["UX Design","Brand Identity","Concepting","Interaction Design"],["Design Systems","Strategy","Digital Experiences","Consulting"]],y=()=>`
+      <div class="our-services">
+        ${h.map(t=>`
+          <div class="services-group">
+            ${t.map(s=>`<a href="#" class="service-item">${s}<svg class="icon-arrow" width="13" height="13"><use href='../img/icons.svg#icon-arrow'></use></svg></a>`).join("")}
+          </div>
+        `).join("")}
+      </div>
+    `,$=document.querySelector("#js-services");$.insertAdjacentHTML("beforeend",y());
 //# sourceMappingURL=index.js.map
