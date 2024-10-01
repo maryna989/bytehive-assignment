7ab342db26b528a4fa13188cb0636527893fc4ed
Maryna Baziliuk <marina.mus24@gmail.com>
2024-10-01 12:12:53 +0300

    add class for social-list(for ul and li) in footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -4,3 +4,10 @@ import { createSocialList } from '../components/createSocialList';
 const footerContainer = document.querySelector('#js-footer');
 
 footerContainer.innerHTML = createSocialList(socialMedia);
+
+const socialIconsList = footerContainer.querySelector('.social-icons-list');
+socialIconsList.classList.add('soclist-icons-footer');
+
+footerContainer
+  .querySelectorAll('.social-item')
+  .forEach(item => item.classList.add('footer-soc-item'));
