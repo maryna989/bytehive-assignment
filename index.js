cf37f8582f53fe97d4526df7b11ec67a58c65eeb
maryna989 <marina.mus24@gmail.com>
2024-10-01 14:02:54 +0000

    Deploying to gh-pages from @ maryna989/bytehive-assignment@b1538aca7cc87ba50db0182c52d42224290e2041 🚀

--- index.js
+++ index.js
@@ -9,12 +9,9 @@
       </li>
     `).join("")}
 `,f=u.map(i=>`<li class="nav-item"><a href="${i.link}">${i.title}</a></li>`).join(""),d=document.querySelector(".nav-list"),m=document.querySelector(".header-socials");d.innerHTML=f;m.innerHTML=a(l);const p=[{title:"People",value:"200"},{title:"Our users the world",value:"50"},{title:"Succesfull projects",value:"100"}],g=p.map((i,t)=>{let o=i.value;return t===0?o=`${o}+`:t===1?o=`${o}K`:t===2&&(o=`${o}%`),`<li class="static-item"><h3>${i.title}</h3><p class="item-value">${o}</p></li>`}).join(""),v=document.querySelector(".js-statistics-list");v.innerHTML=g;const h=[["Creative Direction","Art Direction","Visual Design","Service Design"],["UX Design","Brand Identity","Concepting","Interaction Design"],["Design Systems","Strategy","Digital Experiences","Consulting"]],y=()=>`
-      <div class="our-services">
         ${h.map(i=>`
           <div class="services-group">
             ${i.map(t=>`<a href="#" class="service-item">${t}<svg class="icon-arrow" width="13" height="13"><use href='../img/icons.svg#icon-arrow'></use></svg></a>`).join("")}
-          </div>
-        `).join("")}
-      </div>
-    `,S=document.querySelector("#js-services");S.insertAdjacentHTML("beforeend",y());const n=document.querySelector("#js-footer");n.innerHTML=a(l);const L=n.querySelector(".social-icons-list");L.classList.add("soclist-icons-footer");n.querySelectorAll(".social-item").forEach(i=>i.classList.add("footer-soc-item"));
+            </div>`).join("")}
+    `,S=document.querySelector(".js-our-services");S.innerHTML=y();const n=document.querySelector("#js-footer");n.innerHTML=a(l);const L=n.querySelector(".social-icons-list");L.classList.add("soclist-icons-footer");n.querySelectorAll(".social-item").forEach(i=>i.classList.add("footer-soc-item"));
 //# sourceMappingURL=index.js.map
