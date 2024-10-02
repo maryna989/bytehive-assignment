1fc850fcabcbc2da92d40b849ebe9ad0eea03bf6
Maryna Baziliuk <marina.mus24@gmail.com>
2024-10-02 18:29:00 +0300

    Completed styling for the pricing section

--- src/js/pricing.js
+++ src/js/pricing.js
@@ -3,29 +3,52 @@ import { createFeatureItem } from '../components/planCarts';
 
 const ulElement = document.querySelector('.js-plans-list');
 
+function getClassString(className, isStandard) {
+  return isStandard ? `${className} ${className}-standart` : className;
+}
+
 function createPlanMarkup(plan, index) {
   return `
-            <li class="pricing-items">
-                <div class="plan-header">
-                ${
-                  plan.popular
-                    ? `<span class="most-popular">Most popular</span>`
-                    : ''
-                }
-                    <h3 class="title-plan">${plan.title}</h3> 
-                    <span class="currency">$</span><p class="plan-price">${
-                      plan.price
-                    }</p><span class="month">month</span>
-                    <p class="description">${plan.description}</p>
-                    <button type="button" class="buy-now">Buy now</button>
+            <li class="${getClassString('pricing-items', plan.popular)}">
+                 ${
+                   plan.popular
+                     ? `<div class="most-popular"><span>Most popular</span></div>`
+                     : ''
+                 }
+                <div class="${getClassString('plan-header', plan.popular)}">
+               
+                    <h3 class="${getClassString('title-plan', plan.popular)}">${
+    plan.title
+  }</h3> 
+                    <div class="price-container"><span class="${getClassString(
+                      'currency',
+                      plan.popular
+                    )}">$</span><p class="${getClassString(
+    'plan-price',
+    plan.popular
+  )}">${plan.price}</p><span class="${getClassString(
+    'month',
+    plan.popular
+  )}">month</span>
+                    </div><p class="${getClassString(
+                      'description',
+                      plan.popular
+                    )}">${plan.description}</p>
+                    <button type="button" class="${getClassString(
+                      'buy-now',
+                      plan.popular
+                    )}">Buy now</button>
                     
                 </div>
-
                 <div class="plan-features">
                 
-                 ${index === 0 ? `<p>What’s included</p>` : ''}
+                 ${
+                   index === 0
+                     ? `<p class="plan-features-item">What’s included</p>`
+                     : ''
+                 }
 
-                    <h4 class="plan">${plan.plan}</h4>
+                    <h4 class="plan-title">${plan.plan}</h4>
                     <ul class="list-features">
                         ${plan.features.map(createFeatureItem).join('')}
                     </ul>
