ba67b6abda1b4915259a570a4a3aa8e0de920bef
Maryna Baziliuk <marina.mus24@gmail.com>
2024-10-03 14:53:32 +0300

    test2

--- src/components/createSocialList.js
+++ src/components/createSocialList.js
@@ -6,7 +6,7 @@ export const createSocialList = socialMedia => {
       <li class="social-item">
         <a href="${social.link}" class="social-link" aria-label="${social.platform}">
           <svg class="social-icon" width="26" height="26">
-            <use href="./img/icons.svg${social.icon}"></use>
+            <use href="/img/icons.svg${social.icon}"></use>
           </svg>
         </a>
       </li>
